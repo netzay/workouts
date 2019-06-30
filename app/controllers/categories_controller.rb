8211af4e797583f78b3a8f0a08e61817d1a2f233
netzay <netzelally@gmail.com>
2019-06-30 10:42:59 -0600

    format categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -12,11 +12,11 @@ class CategoriesController < ApplicationController
  	post '/categories' do 
    		@category = Category.create(params[:category])
 		if Workout.valid_entry?(params)
-    		@category.workouts << Workout.create(title: params[:workout][:title], link: params[:workout][:link])
-    	end
-    		@category.save
-    		redirect to "categories/#{@category.id}"
-    	end	
+    	@category.workouts << Workout.create(title: params[:workout][:title], link: params[:workout][:link])
+    end
+    	@category.save
+    	redirect to "categories/#{@category.id}"
+  end	
 
 	get '/categories/index' do
 		if session[:user_id]
@@ -37,30 +37,27 @@ class CategoriesController < ApplicationController
 	get '/categories/:id' do
 		@user = current_user
 		if logged_in?
-    		@category = Category.find(params[:id])
-    		@workout = Workout.find_by(params[:id])	
-		    erb :'/categories/show'
-  		end
+    	@category = Category.find(params[:id])
+    	@workout = Workout.find_by(params[:id])	
+		  erb :'/categories/show'
   	end
+  end
 
-  	post '/categories/:id' do
-  		@category = Category.find(params[:id])
-  		@category.update(params[:category])
-  			if Workout.valid_entry?(params)
-  				@category.workouts << Workout.create(title: params[:workout][:title], link: params[:link][:title])
-  			else
-  				redirect to "/categories/#{@category.id}/edit?error=invalid entry"
-  			end
-    		redirect to '/categories/index'
-    	end
+  post '/categories/:id' do
+  	@category = Category.find(params[:id])
+  	@category.update(params[:category])
+  	if Workout.valid_entry?(params)
+  		@category.workouts << Workout.create(title: params[:workout][:title], link: params[:link][:title])
+  	else
+  		redirect to "/categories/#{@category.id}/edit?error=invalid entry"
+  	end
+    	redirect to '/categories/index'
+  end
     
-
-  
-  	 delete '/categories/:id' do
+  delete '/categories/:id' do
 		if logged_in?
-    		@category = Category.delete(params[:id])
+    	@category = Category.delete(params[:id])
 			redirect to '/categories/index'
     	end
     end
-	
 end
